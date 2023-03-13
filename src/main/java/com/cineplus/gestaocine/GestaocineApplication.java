624263e3799430be1ba5c0574ef61a18fb423352
Stephanie Ingrid <stephanieingrid5@gmail.com>
2023-03-13 13:25:58 -0300

    Criação do projeto

---
+++ src/main/java/com/cineplus/gestaocine/GestaocineApplication.java
@@ -0,0 +1,13 @@
+package com.cineplus.gestaocine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaocineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaocineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cineplus/gestaocine/GestaocineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cineplus.gestaocine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaocineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
